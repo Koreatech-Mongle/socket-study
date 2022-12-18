2c84bca0f96061090bcb527fb4501c91332ffa39
kkx7787 <115688628+kkx7787@users.noreply.github.com>
2022-12-18 17:11:37 +0900

    msg

---
+++ websocket_client/src/index.ts
@@ -0,0 +1,21 @@
+let input = document.querySelector('input[type=file]') as HTMLInputElement;
+var socket = new WebSocket('ws://localhost:8080');
+
+
+socket.addEventListener('open', function(event : any) {
+    socket.send('Hello Server');
+});
+
+function readFile(e : any) {
+    var rawData = e.target.result;
+    socket.send(rawData);
+}
+
+function changeFile() {
+    var file = input?.files?.[0];
+    var reader = new FileReader();
+    reader.addEventListener('load', readFile);
+    reader.readAsArrayBuffer(file);
+}
+
+input?.addEventListener('change', changeFile);
