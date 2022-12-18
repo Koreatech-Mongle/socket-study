39f54b0537f4c76637571764a569c00e67f445e8
kkx7787 <115688628+kkx7787@users.noreply.github.com>
2022-12-18 14:52:11 +0900

    msg

---
+++ tcp_client/src/index.ts
@@ -0,0 +1,29 @@
+const net = require('net');
+
+net.buffersize = 300000;
+net.byteRead = 300000;
+const client = new net.Socket();
+client.connect({port: 8107, host:'localhost'}, function() {
+    console.log(' Connected: ' + 8107 + ':' + 'localhost');
+    client.setTimeout(500);
+    client.on('drain', () =>  {
+        console.log('clinet occured drain >> write buffer is empty');
+    })
+    client.on('data',(data : any) => {
+        console.log(net.bufferSize, data.length);
+        client.end();
+    });
+    client.on('end', () => {
+        console.log(' Client disconnected');
+    });
+    client.on('error', (err : any) => {
+        console.log('Socket Error: ');
+    });
+    client.on('timeout', () => {
+        console.log('Socket Timed Out');
+    });
+    client.on('close', () => {
+        console.log('Socket Closed');
+    });
+
+});
